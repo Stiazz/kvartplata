55ca8c498eefcc5e3df9de77405feb29a00d73bf
Stiazz <Stiazz96@yandex.ru>
2017-03-30 22:08:26 +0500

    Создал класс Transmitter для передачи данных.

---
+++ src/pack1/Transmitter.java
@@ -0,0 +1,19 @@
+package pack1;
+
+public class Transmitter {
+	private static String a = "";
+	private static String b = "";
+	public String getA() {
+		return a;
+	}
+	public void setA(String a) {
+		this.a = a;
+	}
+	public String getB() {
+		return b;
+	}
+	public void setB(String b) {
+		this.b = b;
+	}
+
+}
